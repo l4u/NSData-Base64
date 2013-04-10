4c2aabb5dd47e8db8b7983212910b0a1cbca9a88
Hank Heijink <hank@tumblr.com>
2013-04-10 13:06:25 -0400

    Initialized outputLength variable to satisfy the static analyzer.

--- NSData+Base64.m
+++ NSData+Base64.m
@@ -296,7 +296,7 @@ char *NewBase64Encode(
 //
 - (NSString *)base64EncodedString
 {
-	size_t outputLength;
+	size_t outputLength = 0;
 	char *outputBuffer =
 		NewBase64Encode([self bytes], [self length], true, &outputLength);
 	
@@ -313,7 +313,7 @@ char *NewBase64Encode(
 // added by Hiroshi Hashiguchi
 - (NSString *)base64EncodedStringWithSeparateLines:(BOOL)separateLines
 {
-	size_t outputLength;
+	size_t outputLength = 0;
 	char *outputBuffer =
     NewBase64Encode([self bytes], [self length], separateLines, &outputLength);
 	
